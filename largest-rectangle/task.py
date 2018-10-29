d765a0388cc6c1a396f21682c2ce8b0f87dda71e
alexeykomov <alexeykofficial@gmail.com>
2018-10-29 10:18:05 +0300

    add: updating solution

--- largest-rectangle/task.py
+++ largest-rectangle/task.py
@@ -10,14 +10,17 @@ import sys
 def largest_rectangle(hist):
     stack = []
     max_area = -1
-
+    top = 0
 
     for rectangle_counter in range(0, len(hist)):
-        if len(stack) == 0 or stack[-1] < hist[rectangle_counter]:
-            stack.append(hist[rectangle_counter])
+        if len(stack) == 0 or hist[stack[-1]] < hist[rectangle_counter]:
+            stack.append(rectangle_counter)
         else:
-            while stack[-1] > hist[rectangle_counter]:
-                top
+            while len(stack) and hist[stack[-1]] > hist[rectangle_counter]:
+                top = stack.pop()
+                area = hist[top] * (rectangle_counter - top - 1)
+                if area > max_area:
+                    max_area = area
 
 
 if __name__ == '__main__':
