627bd8c4fa2e0f7ef2f0c5a94f689aeb2db1b2c0
alexeykomov <alexeykofficial@gmail.com>
2019-12-08 14:39:09 +0300

    fix: working castle in the grid

--- castle-on-the-grid/task.py
+++ castle-on-the-grid/task.py
@@ -6,6 +6,10 @@ import random
 import re
 import sys
 from collections import deque
+from os import path
+
+DEBUG = False
+debug_input = deque([])
 
 class Point():
     def __init__(self, x, y):
@@ -16,7 +20,7 @@ class Point():
 
 def get_rose_of_points(n, grid, source_point):
     rose_of_points = []
-    for cursor_x in xrange(0, source_point.x):
+    for cursor_x in xrange(source_point.x - 1, -1, -1):
         if grid[cursor_x][source_point.y] == 'X':
             break
         rose_of_points.append(Point(cursor_x, source_point.y))
@@ -24,7 +28,7 @@ def get_rose_of_points(n, grid, source_point):
         if grid[cursor_x][source_point.y] == 'X':
             break
         rose_of_points.append(Point(cursor_x, source_point.y))
-    for cursor_y in xrange(0, source_point.y):
+    for cursor_y in xrange(source_point.y - 1, -1, -1):
         if grid[source_point.x][cursor_y] == 'X':
             break
         rose_of_points.append(Point(source_point.x, cursor_y))
@@ -42,28 +46,48 @@ def find_shortest_path(n, grid, start_x, start_y, goal_x, goal_y):
         source_point = points_to_visit.popleft()
         current_distance = grid[source_point.x][source_point.y]
         rose_of_points = get_rose_of_points(n, grid, source_point)
-        # print "source_point: " + str(source_point)
-        # print "rose_of_points: " + str(map(str, rose_of_points))
+        if DEBUG:
+            print "source_point: " + str(source_point)
+            print "rose_of_points: " + str(map(str, rose_of_points))
         for point in rose_of_points:
             if grid[point.x][point.y] == '.':
                 grid[point.x][point.y] = current_distance + 1
                 points_to_visit.append(point)
                 if point.x == goal_x and point.y == goal_y:
-                    # print "grid: \n" + "\n".join(map(lambda row: " ".join(map(str, row)), grid))
+                    if DEBUG:
+                        print "grid: \n" + "".join(map(lambda row: " ".join(map(str, row)), grid))
                     return current_distance + 1
-        # print "grid: \n" + "\n".join(map(lambda row: " ".join(map(str, row)), grid))
+        if DEBUG:
+             print "grid: \n" + "".join(map(lambda row: " ".join(map(str, row)), grid))
+             raw_input()
     return -1
 
+def get_input_line():
+    if DEBUG:
+        return debug_input.popleft()
+    return raw_input()
+
 if __name__ == '__main__':
-    n = int(raw_input())
+
+    if DEBUG:
+        script_path = path.dirname(__file__)
+        print "script_path: " + script_path
+        input_path = path.join(script_path, 'input', 'input0.txt')
+        f = open(input_path)
+        line = f.readline()
+        while line:
+            debug_input.append(line)
+            line = f.readline()
+
+    n = int(get_input_line())
 
     grid = []
 
     for _ in xrange(n):
-        grid_items = list(raw_input())
+        grid_items = list(get_input_line())
         grid.append(grid_items)
 
-    start_x, start_y, goal_x, goal_y = map(int, raw_input().split())
+    start_x, start_y, goal_x, goal_y = map(int, get_input_line().split())
     result = find_shortest_path(n, grid, start_x, start_y, goal_x, goal_y)
 
     print str(result) + '\n'
